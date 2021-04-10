b1bcb5628ec7181f163bc1b5779c95eeff7d3ff2
studgradsi2703 <simoncosmin.grad@stud.fh-rosenheim.de>
2021-04-11 01:13:59 +0200

    init

--- server/src/routes/apiV1.js
+++ server/src/routes/apiV1.js
@@ -5,27 +5,27 @@ const router = Router();
 const personen = [];
 
 router.post("/person", (req, res) => {
-    const person = req.body
-    // console.log(req.body);
-    personen.push(person);
+	const person = req.body;
+	// console.log(req.body);
+	personen.push(person);
 
-    res.status(200).json({
-        // personen: personen (kurz:
-        personen
-    })
+	res.status(200).json({
+		// personen: personen (kurz:
+		personen
+	});
 });
 
 router.get("/persons", (req, res) => {
-    res.status(200).json({
-        // personen: personen (kurz:
-        personen
-    })
+	res.status(200).json({
+		// personen: personen (kurz:
+		personen
+	});
 });
 
 const person = {
-    name: "",
-    alter: "",
-    hobbys: []
-}
+	name: "",
+	alter: "",
+	hobbys: []
+};
 
 module.exports = router;
