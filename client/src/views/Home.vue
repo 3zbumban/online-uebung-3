eb700999b4896e1562ed766773d3e4f4463e07aa
3zbumban <37989650+3zbumban@users.noreply.github.com>
2021-11-15 23:43:06 +0100

    asd

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -11,36 +11,34 @@
 
 <script>
 // @ is an alias to /src
-import Persons from '@/components/Persons.vue'
+import Persons from "@/components/Persons.vue";
 
-const baseUrl = "http://localhost:3030/v2"
+const baseUrl = "http://localhost/api/v2";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    Persons
+    Persons,
   },
   data() {
     return {
       persons: false,
       status: {
         msg: "OK",
-        wasBedektes: ""
-      }
-    }
+        wasBedektes: "",
+      },
+    };
   },
   methods: {
     async getPersons() {
       const response = await fetch(baseUrl + "/persons", {
         method: "GET",
-      })
-       const responseJson = await response.json();
-       this.persons = responseJson;
+      });
+      const responseJson = await response.json();
+      this.persons = responseJson;
       //  console.log(responseJson);
     },
-    postPerson() {
-
-    }
-  }
-}
+    postPerson() {},
+  },
+};
 </script>
